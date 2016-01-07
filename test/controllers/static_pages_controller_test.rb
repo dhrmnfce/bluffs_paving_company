a16d31db17544bec9100b7e88d44dab8be452ec8
dhrmnfce <dylanludwick@gmail.com>
2016-01-07 00:32:37 +0000

    Eliminate repitition in static pages controller test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,34 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
+  def setup
+    @base_title = "Bluffs Paving Company"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Bluffs Paving Company"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Bluffs Paving Company"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get history" do
     get :history
     assert_response :success
-    assert_select "title", "History | Bluffs Paving Company"
+    assert_select "title", "History | #{@base_title}"
   end
 
   test "should get careers" do
     get :careers
     assert_response :success
-    assert_select "title", "Careers | Bluffs Paving Company"
+    assert_select "title", "Careers | #{@base_title}"
   end
 
   test "should get projects" do
     get :projects
     assert_response :success
-    assert_select "title", "Projects | Bluffs Paving Company"
+    assert_select "title", "Projects | #{@base_title}"
   end
 
 end
